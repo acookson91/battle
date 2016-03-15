f53c4c3e4fbf8521090b61edbb23d6cdcd94a341
Andrew Cookson <acookson91@gmail.com>
2016-03-15 17:26:41 +0000

    Stored information in cookie

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -7,10 +7,11 @@ Capybara.default_driver = :selenium
 require 'spec_helper'
 
 feature 'Enter names' do
-  scenario 'should submit player 1 name' do
+  scenario 'should submit player 1 and player 2 name' do
     visit('/')
     fill_in('player1', with: 'Andy')
+    fill_in('player2', with: 'Ollie')
     click_button('Submit')
-    expect(page).to have_content 'Andy'
+    expect(page).to have_content 'Andy vs Ollie'
   end
 end
